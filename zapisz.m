930cc2537b9ba2e2c99cd98367a14b61131cca2e
malywladek <malywladek@gmail.com>
2017-05-30 11:25:08 +0200

    Delete zapisz.m

--- zapisz.m
+++
@@ -1,7 +0,0 @@
-function zapisz(name) 
-s=hgexport('readstyle','moje');
-
- fnam=[name,'.png']; % your file name
- s.Format = 'png'; %I needed this to make it work but maybe you wont.
- hgexport(gcf,fnam,s);
-end
